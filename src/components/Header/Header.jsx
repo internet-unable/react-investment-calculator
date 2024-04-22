52205dfe4254b284962e2fd2446ff5347043c23e
internet-unable <polocatic@gmail.com>
2024-04-22 17:03:44 +0300

    Updated Header
    Small changes for Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import img from '../../assets/investment-calculator-logo.png';
+import logo from '../../assets/investment-calculator-logo.png';
 
 export default function Header() {
     return (
         <header id="header">
-            <img src={img} alt="" />
+            <img src={logo} alt="Logo, showing a money bag" />
             <h1>Investment Calculator</h1>
         </header>
     );
